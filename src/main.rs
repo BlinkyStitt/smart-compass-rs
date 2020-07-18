0b38a96149396162ab564460af56fbcadd1a02ed
Bryan Stitt <bryan@stitthappens.com>
2020-07-18 14:32:48 -0700

    i think the spi bus is working

--- src/main.rs
+++ src/main.rs
@@ -17,24 +17,51 @@ pub extern crate feather_m0 as hal;
 
 use hal::prelude::*;
 
+use asm_delay::AsmDelay;
 use hal::clock::GenericClockController;
 use rtic::app;
 
+// TODO: this is a big type
+type Spi = hal::sercom::SPIMaster4<
+    hal::sercom::Sercom4Pad0<hal::gpio::Pa12<hal::gpio::PfD>>,
+    hal::sercom::Sercom4Pad2<hal::gpio::Pb10<hal::gpio::PfD>>,
+    hal::sercom::Sercom4Pad3<hal::gpio::Pb11<hal::gpio::PfD>>
+>;
+
+type SpiMutexInner = core::cell::RefCell<Spi>;
+type SpiMutex = hal::cortex_m::interrupt::Mutex<SpiMutexInner>;
+type SpiBus = shared_bus::CortexMBusManager<SpiMutexInner, Spi>;
+type SpiProxy = shared_bus::proxy::BusProxy<'static, SpiMutex, Spi>;
+
+type SpiRadio = network::Radio<
+    SpiProxy,
+    hal::sercom::Error,
+    hal::gpio::Pa6<hal::gpio::Output<hal::gpio::PushPull>>,
+    hal::gpio::Pa20<hal::gpio::Input<hal::gpio::PullDown>>,
+    hal::gpio::Pa9<hal::gpio::Input<hal::gpio::PullDown>>,
+    hal::gpio::Pa8<hal::gpio::Output<hal::gpio::PushPull>>,
+    (),
+    asm_delay::AsmDelay,
+>;
+
 // static globals
-static MAX_PEERS: u8 = 5;
+// static MAX_PEERS: u8 = 5;
 // the number of ms to offset our network timer. this is time to send+receive+process+draw
-static NETWORK_OFFSET: u16 = 125 + 225;
+// static NETWORK_OFFSET: u16 = 125 + 225;
 static DEFAULT_BRIGHTNESS: u8 = 128;
 static FRAMES_PER_SECOND: u8 = 30;
 
 // TODO: use rtic resources instead
 static mut ELAPSED_MS: usize = 0;
+// Is it possible to use this library with RTIC? - https://github.com/Rahix/shared-bus/issues/4
+static mut SPI_BUS: Option<SpiBus> = None;
 
 #[app(device = hal::pac, peripherals = true)]
 const APP: () = {
     struct Resources {
         every_300_seconds: periodic::Periodic,
         lights: lights::Lights<hal::gpio::Pa15<hal::gpio::Output<hal::gpio::PushPull>>>,
+        radio: SpiRadio,
         red_led: hal::gpio::Pa17<hal::gpio::Output<hal::gpio::OpenDrain>>,
         timer3: hal::timer::TimerCounter3,
         timer4: hal::timer::TimerCounter4,
@@ -68,6 +95,9 @@ const APP: () = {
         let gclk0 = clocks.gclk0();
         let mut pins = hal::Pins::new(device.PORT);
 
+        // let delay = hal::delay::Delay::new(device.SYST, &mut clocks);
+        let delay = AsmDelay::new(asm_delay::bitrate::U32BitrateExt::mhz(48));
+
         let mut timer3 = hal::timer::TimerCounter::tc3_(
             &clocks.tcc2_tc3(&gclk0).unwrap(),
             device.TC3,
@@ -96,32 +126,47 @@ const APP: () = {
         let mut rfm95_int = pins.d3.into_pull_down_input(&mut pins.port);
         // already wired for us
         let mut rfm95_rst = pins.d4.into_push_pull_output(&mut pins.port);
-        let mut led_data = pins.d5.into_push_pull_output(&mut pins.port);
+        let led_data = pins.d5.into_push_pull_output(&mut pins.port);
         // TODO: this pin doesn't actually connect to the radio. is this input type right?
         let mut rfm95_busy_fake = pins.d6.into_pull_down_input(&mut pins.port);
         // already wired for us
         let mut rfm95_cs = pins.d8.into_push_pull_output(&mut pins.port);
         // already wired for us
-        let mut vbat_pin = pins.d9.into_floating_input(&mut pins.port); // also analog
-        let mut sdcard_cs = pins.d10.into_push_pull_output(&mut pins.port);
-        let mut lsm9ds1_csag = pins.d11.into_push_pull_output(&mut pins.port);
-        let mut lsm9ds1_csm = pins.d12.into_push_pull_output(&mut pins.port);
+        // let mut vbat_pin = pins.d9.into_floating_input(&mut pins.port); // also analog
+        // let mut sdcard_cs = pins.d10.into_push_pull_output(&mut pins.port);
+        // let mut lsm9ds1_csag = pins.d11.into_push_pull_output(&mut pins.port);
+        // let mut lsm9ds1_csm = pins.d12.into_push_pull_output(&mut pins.port);
         // already wired for us
-        let mut red_led = pins.d13.into_open_drain_output(&mut pins.port);
-        let mut floating_pin = pins.a0.into_floating_input(&mut pins.port);
+        let red_led = pins.d13.into_open_drain_output(&mut pins.port);
+        // let floating_pin = pins.a0.into_floating_input(&mut pins.port);
 
-        // SPI is shared between radio, sensors, and the SD card
-        // TODO: what speed? m4 maxes at 24mhz. is m0 the same? what 
-        let my_spi = hal::spi_master(
-            &mut clocks,
-            24.mhz(),
-            device.SERCOM4,
-            &mut device.PM,
-            pins.sck,
-            pins.mosi,
-            pins.miso,
-            &mut pins.port,
-        );
+        // TODO: use a dummy Mutex so that rtic is happy
+        // https://github.com/Rahix/shared-bus/issues/4#issuecomment-653635843
+        let spi_bus = {
+            // SPI is shared between radio, sensors, and the SD card
+            // TODO: what speed? m4 maxes at 24mhz. is m0 the same? what 
+            let my_spi = hal::spi_master(
+                &mut clocks,
+                24.mhz(),
+                device.SERCOM4,
+                &mut device.PM,
+                pins.sck,
+                pins.mosi,
+                pins.miso,
+                &mut pins.port,
+            );
+
+            let spi_bus = shared_bus::CortexMBusManager::new(my_spi);
+
+            unsafe {
+                SPI_BUS = Some(spi_bus);
+                // This reference is now &'static
+                SPI_BUS.as_ref().unwrap()
+            }
+        };
+
+        // setup the radio
+        let my_radio = network::Radio::new(spi_bus.acquire(), rfm95_cs, rfm95_busy_fake, rfm95_int, rfm95_rst, delay);
 
         // setup serial for communicating with the gps module
         // TOOD: SERCOM0 or SERCOM1?
@@ -137,16 +182,7 @@ const APP: () = {
         );
 
         // create lights
-        // TODO: 
         let my_lights = lights::Lights::new(led_data, DEFAULT_BRIGHTNESS, FRAMES_PER_SECOND);
-
-        // TODO: setup radio
-        // TODO: what should delay be?
-        // TODO: i think that we need shared-bus to share the spi
-        // let mut my_radio = network::Radio::new(my_spi, rfm95_cs, rfm95_busy_fake, rfm95_int, rfm95_rst, delay);
-
-        // TODO: setup compass/orientation sensor
-        // TODO: setup sd card
 
         // TODO: setup setup gps
         // TODO: the adafruit_gps crate requires std::io! looks like we need to roll our own
@@ -160,6 +196,7 @@ const APP: () = {
         init::LateResources {
             every_300_seconds,
             lights: my_lights,
+            radio: my_radio,
             // rfm95_int: pins.d3.into_pull_down_input(&mut pins.port),
             // already wired for us
             // rfm95_rst: pins.d4.into_push_pull_output(&mut pins.port),
@@ -182,13 +219,23 @@ const APP: () = {
     }
 
     // `shared` cannot be accessed from this context
+    // TODO: more of this should probably be done with interrupts
     #[idle(resources = [
         every_300_seconds,
         lights,
+        radio,
     ])]
     fn idle(c: idle::Context) -> ! {
         let every_300_seconds = c.resources.every_300_seconds;
         let my_lights = c.resources.lights;
+
+        // TODO: setup radio
+        // TODO: what should delay be?
+        // TODO: i think that we need shared-bus to share the spi
+
+        // TODO: setup compass/orientation sensor
+        // TODO: setup sd card
+
 
         loop {
             if every_300_seconds.ready() {
